b2d69fec8a99693c48c55a0a9b0940c25f9eaeeb
Invers3 <manyandamariano@hotmail.com>
2025-03-03 13:35:04 -0500

    Update Footer.js
    Adding styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ export default function Header(){
     return <div id="footer">
         <div className="content">
             <ul className="list">
-                <li><a href="/location">Ubicación</a></li>
                 <li><a target="_blank" href="https://www.facebook.com/rimaymanta">Facebook</a></li>
             </ul>
             <br></br>
