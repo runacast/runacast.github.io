c29c0617310e96e2c3a891c33c82ba8b1c52440f
Invers3 <manyandamariano@hotmail.com>
2025-03-07 16:19:19 -0500

    Update Header.js
    Testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ export default function Header(){
                     </label>
                 </div>
                 <div className="nav-side navbar-toggle">
-                    <ul className="menu" link={rootDir} href={filePath}>
+                    <ul className="menu">
                     {menu.map((item, index) => (
                         <li key={index}><a href={item.slug}>{item.title}</a></li>
                     ))}
