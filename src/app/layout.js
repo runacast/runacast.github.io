91d6be20f76e90c40346968165495d4831e3d44e
Invers3 <manyandamariano@hotmail.com>
2025-03-07 17:30:01 -0500

    Update pages.js
    Testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   const rootDir = process.cwd()
-  const pal = path.join(rootDir, 'src', 'posts')
+  const pal = path.join(rootDir, 'src', 'pages')
   const apl = fs.readdirSync(pal)
 
   return (
