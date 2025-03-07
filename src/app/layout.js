6e606785a1a9fca394022662da21ba26a69a0fd7
Invers3 <manyandamariano@hotmail.com>
2025-03-07 18:49:30 -0500

    Check menu
    testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   const rootDir = process.cwd()
-  const pal = path.join(rootDir, 'src', 'menus')
+  const pal = path.join(rootDir)
   const apl = fs.readdirSync(pal)
 
   return (
