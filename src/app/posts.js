d6d2efddc16eb7c0ec18bb7efb19dc5066c301e2
Invers3 <manyandamariano@hotmail.com>
2025-03-07 23:22:54 -0500

    Update dir
    Testing

---
+++ src/app/posts.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const postsFolder = path.join(process.cwd(), 'src', 'posts')
+const outputFile = path.join(process.cwd(), 'src', 'index', 'lasted.json')
+const postFiles = fs.readdirSync(postsFolder).filter((file) => file.endsWith('.json'))
+let lasted = []
+
+const postsList = postFiles.map((file, index) => {
+
+    if(index <= 9){
+        const filePath = path.join(postsFolder, file)
+        const fileContent = fs.readFileSync(filePath, 'utf8')
+        lasted.push(JSON.parse(fileContent))
+    }
+
+})
+
+fs.writeFileSync(outputFile, JSON.stringify(lasted, null, 2))
