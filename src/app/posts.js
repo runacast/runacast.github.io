6dfd1dbd35cf746f8c4f3ae4ec2460c44a7734f2
Invers3 <manyandamariano@hotmail.com>
2025-04-07 22:32:41 -0500

    Update posts.json
    Testing

--- src/app/posts.js
+++ src/app/posts.js
@@ -3,17 +3,20 @@ const path = require('path')
 
 const postsFolder = path.join(process.cwd(), 'src', 'posts')
 const outputFile = path.join(process.cwd(), 'src', 'index', 'lasted.json')
-const postFiles = fs.readdirSync(postsFolder).filter((file) => file.endsWith('.json'))
+const postFiles = fs.readdirSync(postsFolder)
+
 let lasted = []
 
-/*const postsList = postFiles.map((file, index) => {
-
-    if(index <= 9){
-        const filePath = path.join(postsFolder, file)
-        const fileContent = fs.readFileSync(filePath, 'utf8')
-        lasted.push(JSON.parse(fileContent))
+const postsList = postFiles.map((file, index) => {
+    
+    if(file.match(/^.*\.json$/)){
+        if(index <= 9){
+            const filePath = path.join(postsFolder, file)
+            const fileContent = fs.readFileSync(filePath, 'utf8')
+            lasted.push(JSON.parse(fileContent))
+        }
     }
-
-})*/
+    
+})
 
 fs.writeFileSync(outputFile, JSON.stringify(lasted, null, 2))
